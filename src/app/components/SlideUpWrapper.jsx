599ad01967fb95d161fec00723e8261ba1950073
Huzaifa Shuaib <huzaifashoaib7@gmail.com>
2025-07-30 15:48:19 +0500

    fix:animation fixes

--- src/app/components/SlideUpWrapper.jsx
+++ src/app/components/SlideUpWrapper.jsx
@@ -5,7 +5,7 @@ const SlideUpWrapper = ({ children, className = "", id }) => {
     <motion.div
       initial={{ opacity: 0, y: 80 }}
       whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true, amount: 0.1 }}
+      viewport={{ once: true, amount: 1 }}
       transition={{ duration: 1.2, ease: "easeInOut", delay: 0 }}
       className={`will-change-transform ${className}`}
       id={id}
